8d3e03c76fda6bc55cfa587270ba5ea9b1339266
najmussakib2142 <najmussakib2142@gmail.com>
2025-08-20 03:45:29 +0600

    create firebase contexts

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from './App.jsx'
 import { RouterProvider } from "react-router/dom";
 import { router } from './router/router.jsx';
 import { ThemeProvider } from './provider/ThemeContext.jsx';
+import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-roboto max-w-7xl mx-auto'>
       <ThemeProvider>
-        <RouterProvider router={router} />
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
       </ThemeProvider>
     </div>
   </StrictMode>,
